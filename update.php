3e8490f65faf58b78ae55fe414c58bf6dfea5588
mitchellurgero <mitchellurgero@gmail.com>
2015-01-13 13:27:33 -0600

    Update update.php

--- update.php
+++ update.php
@@ -2,6 +2,9 @@
 $CurrentVersion='7'; //Version Number.
 $DownloadPath='http://domain.com/ota/path_to_update.zip'; // Path to the update.zip Download.
 $BasicRomInfo='Some ROM Info here.'; // Limited to 250 Characters for now.
+//
+//DO NOT MODIFY ANYTHING PAST THIS POINT, UNLESS YOU KNOW WHAT YOU ARE DOING.
+//
 $GETCom = htmlspecialchars($_GET["push"]);
 if ($GETCom == "DownloadUpdate") {
 header( 'Location: '.$DownloadPath );
